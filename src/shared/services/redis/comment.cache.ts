360a0befacce0fbec59c0078565981492dd90361
TuanMinh7979 <minhtuanto179@gmail.com>
2023-07-21 15:27:40 +0700

    complete comment redis

--- src/shared/services/redis/comment.cache.ts
+++ src/shared/services/redis/comment.cache.ts
@@ -5,14 +5,17 @@ import Logger from "bunyan";
 import { config } from "@root/config";
 import { ServerError } from "@global/helpers/error-handler";
 import { Helpers } from "@global/helpers/helper";
-import { ICommentDocument } from "@comment/interfaces/comment.interface";
+import {
+  ICommentDocument,
+  ICommentNameList,
+} from "@comment/interfaces/comment.interface";
 const log: Logger = config.createLogger("commentsCache");
 export class CommentCache extends BaseCache {
   constructor() {
     super("commentCache");
   }
-//   * Params: postId(postId want comment to), value(text of comment)
-//   * Res: void
+  //   * Params: postId(postId want comment to), value(text of comment)
+  //   * Res: void
   public async savePostCommentToCache(
     postId: string,
     value: string
@@ -36,8 +39,8 @@ export class CommentCache extends BaseCache {
     }
   }
 
-//   * Params: postId
-//   * Res:comments:key List Object of a post in cache
+  //   * Params: postId
+  //   * Res:comments:key List Object of a post in cache
   public async getCommentsFromCache(
     postId: string
   ): Promise<ICommentDocument[]> {
@@ -60,6 +63,74 @@ export class CommentCache extends BaseCache {
     } catch (e) {
       log.error(e);
       throw new ServerError("Server error, try again");
+    }
+  }
+
+  //   * Params: postId
+  //   * Res:ICommentNameList[] but actual only element
+  public async getUserNamesOfACommentFromCache(
+    postId: string
+  ): Promise<ICommentNameList[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const commentsCount: number = await this.client.LLEN(
+        `comments:${postId}`
+      );
+      const comments: string[] = await this.client.LRANGE(
+        `comments:${postId}`,
+        0,
+        -1
+      );
+      const list: string[] = [];
+
+      for (const item of comments) {
+        const comment: ICommentDocument = Helpers.parseJson(
+          item
+        ) as ICommentDocument;
+        list.push(comment.username);
+      }
+      const aCommentNameList: ICommentNameList = {
+        count: commentsCount,
+        names: list,
+      };
+      return [aCommentNameList];
+    } catch (e) {
+      log.error(e);
+      throw new ServerError("Server error, try again");
+    }
+  }
+
+  // * Params:
+  // * postId
+  // * commentId
+  //   * Res:ICommentNameList[] but actual only element
+  public async getACommentFromCache(
+    postId: string,
+    commentId: string
+  ): Promise<ICommentDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const comments: string[] = await this.client.LRANGE(
+        `comments:${postId}`,
+        0,
+        -1
+      );
+      const list: ICommentDocument[] = [];
+      for (const item of comments) {
+        list.push(Helpers.parseJson(item));
+      }
+      const result: ICommentDocument = list.find(
+        (listItem: ICommentDocument) => listItem._id === commentId
+      ) as ICommentDocument;
+
+      return [result];
+    } catch (error) {
+      log.error(error);
+      throw new ServerError("Server error. Try again.");
     }
   }
 }

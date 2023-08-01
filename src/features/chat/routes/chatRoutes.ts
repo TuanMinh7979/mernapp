24eedde1c93815647450930e6cd3d740d15fa030
TuanMinh7979 <minhtuanto179@gmail.com>
2023-08-01 13:19:22 +0700

    soft delete message

--- src/features/chat/routes/chatRoutes.ts
+++ src/features/chat/routes/chatRoutes.ts
@@ -1,9 +1,8 @@
-import { Add } from '@chat/controllers/add-chat-messages';
-import { Get } from '@chat/controllers/get-chat-message';
-import { authMiddleware } from '@global/helpers/aurth-middleware';
-import express, { Router } from 'express';
-
-
+import { Add } from "@chat/controllers/add-chat-messages";
+import { Delete } from "@chat/controllers/delete-chat-message";
+import { Get } from "@chat/controllers/get-chat-message";
+import { authMiddleware } from "@global/helpers/aurth-middleware";
+import express, { Router } from "express";
 
 class ChatRoutes {
   private router: Router;
@@ -13,13 +12,36 @@ class ChatRoutes {
   }
 
   public routes(): Router {
- 
-    this.router.get('/chat/message/conversations', authMiddleware.checkAuthencation, Get.prototype.conversationList);
-    this.router.get('/chat/message/user/:receiverId', authMiddleware.checkAuthencation, Get.prototype.messages);
-    this.router.post('/chat/message', authMiddleware.checkAuthencation, Add.prototype.message);
-    this.router.post('/chat/message/add-chat-users', authMiddleware.checkAuthencation, Add.prototype.addChatUsers);
-    this.router.post('/chat/message/remove-chat-users', authMiddleware.checkAuthencation, Add.prototype.removeChatUsers);
-
+    this.router.get(
+      "/chat/message/conversations",
+      authMiddleware.checkAuthencation,
+      Get.prototype.conversationList
+    );
+    this.router.get(
+      "/chat/message/user/:receiverId",
+      authMiddleware.checkAuthencation,
+      Get.prototype.messages
+    );
+    this.router.post(
+      "/chat/message",
+      authMiddleware.checkAuthencation,
+      Add.prototype.message
+    );
+    this.router.post(
+      "/chat/message/add-chat-users",
+      authMiddleware.checkAuthencation,
+      Add.prototype.addChatUsers
+    );
+    this.router.post(
+      "/chat/message/remove-chat-users",
+      authMiddleware.checkAuthencation,
+      Add.prototype.removeChatUsers
+    );
+    this.router.delete(
+      "/chat/message/mark-as-deleted/:messageId/:senderId/:receiverId/:type",
+      authMiddleware.checkAuthencation,
+      Delete.prototype.markMessageAsDeleted
+    );
 
     return this.router;
   }
--- src/shared/services/queue/chat.queue.ts
+++ src/shared/services/queue/chat.queue.ts
@@ -7,7 +7,7 @@ class ChatQueue extends BaseQueue {
   constructor() {
     super('chats');
     this.processJob('addChatMessageToDB', 5, chatWorker.addChatMessageToDB);
-    // this.processJob('markMessageAsDeletedInDB', 5, chatWorker.markMessageAsDeleted);
+    this.processJob('markMessageAsDeletedInDB', 5, chatWorker.markMessageAsDeleted);
     // this.processJob('markMessagesAsReadInDB', 5, chatWorker.markMessagesAsReadInDB);
     // this.processJob('updateMessageReaction', 5, chatWorker.updateMessageReaction);
   }

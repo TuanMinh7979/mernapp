d2f7dec5be8966690bb5ca70e343ffcb8f02efb8
TuanMinh7979 <minhtuanto179@gmail.com>
2023-08-04 00:21:02 +0700

    health routes

--- src/routes.ts
+++ src/routes.ts
@@ -12,13 +12,19 @@ import { notificationRoutes } from "@notification/routes/notificationRoutes";
 import { imageRoutes } from "@image/routes/imageRoutes";
 import { chatRoutes } from "@chat/routes/chatRoutes";
 import { userRoutes } from "@user/routes/userRoute";
+import { healthRoutes } from "@user/routes/healthRoute";
 const BASE_PATH = "/api/v1";
 export default (app: Application) => {
   const routes = () => {
     app.use("/queues", serverAdapter.getRouter());
+
+    app.use("", healthRoutes.health());
+    app.use("", healthRoutes.env());
+    // app.use("", healthRoutes.instance());
+    // app.use("", healthRoutes.fiboRoutes());
+
     app.use(BASE_PATH, authRoutes.routes());
     app.use(BASE_PATH, authRoutes.signoutRoute());
-
     app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
     app.use(BASE_PATH, authMiddleware.verifyUser, postRoutes.routes());
     app.use(BASE_PATH, authMiddleware.verifyUser, reactionRoutes.routes());
@@ -27,7 +33,7 @@ export default (app: Application) => {
     app.use(BASE_PATH, authMiddleware.verifyUser, notificationRoutes.routes());
     app.use(BASE_PATH, authMiddleware.verifyUser, imageRoutes.routes());
     app.use(BASE_PATH, authMiddleware.verifyUser, chatRoutes.routes());
-    app.use(BASE_PATH, authMiddleware.verifyUser,userRoutes.routes());
+    app.use(BASE_PATH, authMiddleware.verifyUser, userRoutes.routes());
   };
   routes();
 };
